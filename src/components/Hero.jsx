187918af4ffd9ffba2c811e39a8d584a6b5810af
Sohaib Ahmed <dev.sohaibahmed@gmail.com>
2022-09-21 17:14:00 -0400

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import resume from '../assets/SohaibResume.pdf'
 
 const Hero = () => {
   return (
-    <section id='home' className="lg:h-[85vh] flex items-center bg-gray-800 lg:bg-cover lg:bg-center lg-bg-no-repeat py-32 lg:py-0 overflow-hidden">
+    <section id='home' className="lg:h-[85vh] flex items-center bg-gray-800 lg:bg-cover lg:bg-center lg-bg-no-repeat py-32 px-16 lg:py-0 overflow-hidden">
         <div className="container mx-auto">
             <div className="flex items-center h-full pt-8">
               <div className="flex-1 flex flex-col items-center lg:items-start">
